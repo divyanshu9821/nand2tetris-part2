d272a91797aee2c132a383399ca5eb45f9b0ac17
Divyanshu Singhal <96660033+divyanshu9821@users.noreply.github.com>
2026-03-24 15:38:40 +0530

    Update vmTranslator.js

--- vmTranslator.js
+++ vmTranslator.js
@@ -1,294 +1,54 @@
-const stateMap = {
-    opcode: 'opcode',
-    comment: 'comment',
-    segment: 'segment',
-    value: 'value'
+const stackOpcode = ['push', 'pop']
+
+const aluOpcode = [
+    'gt', 'lt', 'eq',
+    'neg', 'add', 'sub',
+    'and', 'or', 'not'
+]
+
+const segments = [
+    'constant', 'local', 'argument',
+    'static', 'this', 'that',
+    'temp', 'pointer',
+]
+
+const cleanLine = line => line.split('/')[0].trim()
+const extractTokens = line => line.split(/\s+/)
+
+const error = msg => { throw new Error(msg) }
+
+function validateAluOpcode(tokens) {
+    if (!aluOpcode.includes(tokens[0])) error('wrong opcode')
 }
 
-const opcodeMap = {
-    push: 'push', pop: 'pop',
-    gt: 'gt', lt: 'lt', eq: 'eq',
-    neg: 'neg', add: 'add', sub: 'sub',
-    and: 'and', or: 'or', not: 'not',
-    has: (opcode) => {
-        if (opcodeMap[opcode] == undefined) throw new Error('wrong opcode')
-    }
+function validateStackOpcode(tokens) {
+    if (!stackOpcode.includes(tokens[0])) error('wrong opcode')
+    if (!segments.includes(tokens[1])) error('wrong segment')
+    if (Number.isNaN(Number(tokens[2]))) error('wrong value')
 }
 
-const segmentMap = {
-    constant: 'constant', local: 'local', argument: 'argument',
-    static: 'static', thiss: 'this', that: 'that',
-    temp: 'temp', pointer: 'pointer',
-    has: (segment) => {
-        if (segmentMap[segment] == undefined) throw new error('wrong segment')
-    }
+function processLines(line) {
+    line = cleanLine(line)
+    if (line == "") return
+
+    const tokens = extractTokens(line)
+
+    if (tokens.length == 1) validateAluOpcode(tokens)
+    else if (tokens.length == 3) validateStackOpcode(tokens)
+    else return;
+
+    console.log(tokens) // translate here
+
 }
-
-function stateFactory() {
-    let _state = null;
-
-    function reset() {
-        _state = stateMap.opcode
-    }
-
-    function set(st) {
-        _state = st
-    }
-
-    function get() {
-        return _state;
-    }
-
-    function is(st) {
-        return _state == st
-    }
-
-    reset();
-
-    return { reset, set, get, is }
-}
-
-function bufferFactory() {
-    let _buf = null;
-
-    function reset() {
-        _buf = '';
-    }
-
-    function getString() {
-        return _buf.toLowerCase();
-    }
-
-    function getNumber() {
-        return Number(_buf)
-    }
-
-    function append(ch) {
-        if (ch != ' ') _buf += ch;
-    }
-
-    function isEmpty() {
-        return _buf.trim() == ''
-    }
-
-    reset();
-
-    return { reset, getString, getNumber, append, isEmpty }
-}
-
-function expressionFactory() {
-    let opcode = null
-    let segment = null
-    let value = null
-
-    function reset() {
-        opcode = ''
-        segment = ''
-        value = ''
-    }
-
-    function get() {
-        return { opcode, segment, value }
-    }
-
-    function setOpcode(state, buffer) {
-        if (!state.is(stateMap.opcode)) return
-        if (buffer.isEmpty()) return
-        opcode = buffer.getString()
-        opcodeMap.has(opcode)
-        buffer.reset();
-        state.set(stateMap.segment)
-    }
-
-    function setSegment(state, buffer) {
-        if (!state.is(stateMap.segment)) return
-        if (buffer.isEmpty()) return
-        segment = buffer.getString()
-        segmentMap.has(segment)
-        buffer.reset();
-        state.set(stateMap.value)
-    }
-
-    function setValue(state, buffer) {
-        if (!state.is(stateMap.value)) return;
-        if (buffer.isEmpty()) return;
-        value = buffer.getString()
-        buffer.reset();
-        state.set(stateMap.opcode)
-    }
-
-    function final(state, buffer) {
-        if (opcode == '') return;
-
-        if (opcode == opcodeMap.push || opcode == opcodeMap.pop)
-            if (segment.trim() == '' || value.trim() == '')
-                throw new Error('vm code is not correct')
-
-        state.reset()
-        buffer.reset()
-        const expObj = get();
-        reset()
-        return expObj;
-    }
-
-    reset()
-
-    return { reset, get, setOpcode, setSegment, setValue, final }
-}
-
-const state = stateFactory()
-const buffer = bufferFactory()
-const expression = expressionFactory()
-
-function processCharacter(ch) {
-    if (ch == " ") {
-
-        expression.setOpcode(state, buffer)
-        expression.setSegment(state, buffer)
-
-    } else if (ch == "\n" || ch == '/') {
-        if (state.is(stateMap.comment)) {
-            if(ch == "\n") state.set(stateMap.opcode)
-            else return
-        }
-        expression.setOpcode(state, buffer)
-        expression.setValue(state, buffer)
-
-        const expObj = expression.final(state, buffer)
-
-        console.log(expObj)
-
-        if (ch == "/") {
-            state.set(stateMap.comment)
-        }
-    }
-    else {
-
-        if (state.is(stateMap.comment) || ch == '\r') return
-
-        buffer.append(ch)
-    }
-}
-
-
-
-const { error } = require('console')
-const fs = require('fs')
 
 const inputFile = process.argv[2]
-const outputFile = process.argv[3] ?? 'output.asm'
+if (!inputFile) return console.log('please provide input file')
 
-if (!inputFile) throw new Error('provide the vm code file for translation')
+const fs = require('fs')
+const content = fs.readFileSync(inputFile, 'utf8')
 
-const stream = fs.createReadStream(inputFile, { encoding: 'utf8' })
+const lines = content.split('\n')
 
-stream.on('data', chunk => {
-    for (let ch of chunk) {
-        processCharacter(ch)
-    }
-})
-
-stream.on('end', () => {
-    processCharacter('\n')
-})
-
-// let state = {
-//     op: 0,
-//     seg: 1,
-//     val: 3,
-//     com: 4,
-//     _val: null,
-//     reset() {
-//         state._val = state.op
-//     },
-//     isOpcode() {
-//         return state._val == state.op
-//     },
-//     comment() {
-//         state._val = state.com
-//     },
-//     isComment() {
-//         return state._val == state.com
-//     },
-//     value() {
-//         state._val = state.val
-//     },
-//     isValue() {
-//         return state._val == state.val
-//     }
-
-// }
-// state.reset();
-
-// let buffer = {
-//     value: null,
-//     reset() {
-//         buffer.value = ''
-//     },
-//     append(ch) {
-//         if (ch != ' ') buffer.value += ch
-//     },
-//     isEmpty() {
-//         return buffer.value == ''
-//     }
-// }
-// buffer.reset()
-
-// let expression = {
-//     op: null,
-//     seg: null,
-//     val: null,
-//     reset() {
-//         expression.op = ''
-//         expression.seg = ''
-//         expression.val = ''
-//     },
-//     opcode(op) {
-//         expression.op = op
-//     },
-//     segment(seg) {
-//         expression.seg = seg
-//     },
-//     value(val) {
-//         expression.val = val
-//     }
-// }
-// expression.reset()
-
-// module.exports.processChunk = function (chunk) {
-//     for (const ch of chunk) {
-//         if (ch == ' ') {
-        
-//             if (state.isComment()) return;
-        
-//         } else if (ch == '/') {
-            
-//             if (state.isComment()) return;
-            
-//             if (buffer.isEmpty()){
-//                 state.comment();
-//                 return;
-//             }
-            
-//             if (state.isOpcode()) {
-
-//             }
-        
-//         } else if (ch == '\n') {
-        
-//             state.reset();
-        
-//             if (buffer.isEmpty()) return;
-        
-//             console.log(expression.opcode, expression.segment, expression.value)
-        
-//         } else {
-        
-//             if (state.isComment()) return;
-        
-//             if (ch == '\r') return;
-        
-//             buffer.append(ch)
-//         }
-//     }
-// }
+for (const line of lines) {
+    processLines(line)
+}
